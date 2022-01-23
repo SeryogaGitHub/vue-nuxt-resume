52f5d0630653e58e05cfd87e3093762493f8a6dc
Seryoga <work.email.seryoga@gmail.com>
2022-01-23 15:18:39 +0200

    change style

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div class="wrapper">
     <Navbar/>
     <nuxt/>
     <Footer/>
--- pages/contacts.vue
+++ pages/contacts.vue
@@ -5,10 +5,10 @@
         Контакти
       </h1>
 
-      <div class="pane wow flipInX ">
+      <div class="pane">
         <h4>
           <span class="gold-text">Місто проживання: </span>
-          Житомир
+          <a target="_blank" href="https://www.google.com/maps/place/%D0%96%D0%B8%D1%82%D0%BE%D0%BC%D0%B8%D1%80,+%D0%96%D0%B8%D1%82%D0%BE%D0%BC%D0%B8%D1%80%D1%81%D1%8C%D0%BA%D0%B0+%D0%BE%D0%B1%D0%BB%D0%B0%D1%81%D1%82%D1%8C,+10000/@50.2678654,28.603678,12z/data=!3m1!4b1!4m5!3m4!1s0x472c64a32bfa355d:0xf14ad2a3d9b9e229!8m2!3d50.25465!4d28.6586669">Житомир</a>
         </h4>
 
         <h4>
@@ -18,18 +18,17 @@
 
         <h4>
           <span class="gold-text">Телефон: </span>
-          +380 98 96 96 952
+          <a href="tel:+380 98 96 96 952" class="text-white">+380 98 96 96 952</a>
         </h4>
 
         <h4>
           <span class="gold-text">Email: </span>
-          main.email.seryoga@gmail.com
+          <a href="mailto:main.email.seryoga@gmail.com" class="text-white">main.email.seryoga@gmail.com</a>
         </h4>
 
         <h4>
           <span class="gold-text">Робочі години: </span>
-          Пн-Пт: 10:00 - 19:00 <br/>
-          <span class="gold-text">Для пропозицій від яких не зможу відмовитися, на зв`язку цілодобово :)</span>
+          Пн-Пт: 10:00 - 17:00
         </h4>
       </div>
 
